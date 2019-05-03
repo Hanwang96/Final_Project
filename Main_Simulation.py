c2f291e6c4e6907b878c3ca7843dfd3dee6768c7
HanWang <hanw7@illinois.edu>
2019-05-03 11:29:12 -0500

    debug

--- Main_Simulation.py
+++ Main_Simulation.py
@@ -11,8 +11,10 @@ def compute():
     std_list_inner = []
     num_list_inner = []
 
-    for i in range(50, 300, 50):
-        for j in range(1, 6):
+    for i in range(50,300,50):
+        for j in range(1,6):
+            i = str(i)
+            j = str(j)
             filename = "result_" + i + "_"+j+".csv"
             data = pd.read_csv(filename)
             stays = data[data['status'] > 0]
@@ -38,24 +40,28 @@ def MC_result_MMR_avg(avg_list):
     y = avg_list
     plt.figure()
     plt.plot(x, y)
+    plt.show()
 
 def MC_result_MMR_std(std_list):
     x = np.linspace(50, 300, 50)
     y = std_list
     plt.figure()
     plt.plot(x, y)
+    plt.show()
 
 def MC_result_num(num_list):
     x = np.linspace(50, 300, 50)
     y = num_list
     plt.figure()
     plt.plot(x, y)
+    plt.show()
 
 if __name__ == "__main__":
     filelist = compute()
     avglist = filelist[0]
     stdlist = filelist[1]
     numlist = filelist[2]
+
     MC_result_MMR_avg(avglist)
     MC_result_MMR_std(stdlist)
     MC_result_num(numlist)
